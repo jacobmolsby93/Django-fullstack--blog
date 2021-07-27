f6b88511231aa4629e91c47ffd785b831f2b6edc
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-27 20:02:05 +0000

    Add functionality to posts, post overview and link to post page.

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post
 
+# Django generic view.
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    # Limiting the number of post that can appear on the front page.
+    paginate_by = 6
+    # If ther is more than 6 django will automatically create a page navigation.
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+            },
+        )
